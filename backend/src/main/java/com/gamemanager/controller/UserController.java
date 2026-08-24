5a091429483451f74d7e4b2a73e146c22a7f0028
bnoble1986 <bnoblevisuals@gmail.com>
2026-08-24 19:55:38 -0400

    feat: validate registration data

--- backend/src/main/java/com/gamemanager/controller/UserController.java
+++ backend/src/main/java/com/gamemanager/controller/UserController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gamemanager.dto.RegistrationRequest;
-
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,8 +15,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/api/users")
 public class UserController {
     @PostMapping("/register")
-public ResponseEntity<String> register(@RequestBody RegistrationRequest request)
-    {
+public ResponseEntity<String> register(@RequestBody @Valid RegistrationRequest request)
+    {   
         return ResponseEntity.status(HttpStatus.CREATED).body(
             "User registration received"
         );
--- backend/src/main/java/com/gamemanager/dto/RegistrationRequest.java
+++ backend/src/main/java/com/gamemanager/dto/RegistrationRequest.java
@@ -1,8 +1,25 @@
 package com.gamemanager.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+//import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+
 public class RegistrationRequest {
+    @NotBlank
+    @Size(max=50)
     private String userName;
+
+    @NotBlank
+    @Size(min=8,max=255)
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[^A-Za-z\\d]).+$")
     private String password;
+
+    @NotBlank
+    @Size(max=255)
+    @Email
     private String email;
 
     public String getUserName(){
@@ -13,19 +30,19 @@ public class RegistrationRequest {
         this.userName = userName;
     }
 
-    public String getUserPassWord(){
+    public String getPassword(){
         return password;
     }
 
-    public void setUserPassword(String password){
+    public void setPassword(String password){
         this.password = password;
     }
 
-    public String getUserEmail(){
+    public String getEmail(){
         return email;
     }
 
-    public void setUserEmail(String email){
+    public void setEmail(String email){
         this.email = email;
     }
 
